95ef1c00e2530ad14fd509732d4ee6ff64f71213
Matthew <barbaramatthew90@gmail.com>
2018-09-28 10:18:39 +0200

    updated backend URL

--- fe/src/constants/index.js
+++ fe/src/constants/index.js
@@ -1,8 +1,8 @@
 export const GEO_CODE_API_KEY = "AIzaSyCIKud8HrdeCkRdBRZ2oW835Hb3jSqMcvU";
 export const API_GET_MUNICIPALITIES =
-  "http://localhost:3000/address/municipalities";
+  "http://localhost:4000/address/municipalities";
 export const API_GET_ADDRESS_BY_POSTCODE =
-  "http://localhost:3000/address/postcode-number";
+  "http://localhost:4000/address/postcode-number";
 export const CHANGE_CURRENT_ADDRESS = "CHANGE_CURRENT_ADDRESS";
 export const FORM_MUNICIPALITY = "MUNICIPALITY_SUGGESTED";
 export const FORM_LOCATION = "FORM_LOCATION";
