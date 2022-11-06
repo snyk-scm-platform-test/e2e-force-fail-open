cb30d27993bf65e7b8244fd94353b7b78ab2280a
Matthew Barbara <matthew.barbara@snyk.io>
2022-11-06 22:35:40 +0100

    testing PR checks

--- be/index.js
+++ be/index.js
@@ -7,24 +7,30 @@ const db = require("./database");
 const app = express();
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+
   next();
 });
 
-app.use("/address/postcode-number/:number", function(req, res) {
+app.use("/address/postcode-number/:number", function (req, res) {
   res.setHeader("Content-Type", "application/json");
 
   db.getAddressByPostcodeNumber(req.params.number)
-    .then(result => {
+    .then((result) => {
       res.end(JSON.stringify(result, null, 2));
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500);
       res.end(
         JSON.stringify({
           errorCode: 500,
-          message: error
+          message: error,
         }),
         null,
         2
